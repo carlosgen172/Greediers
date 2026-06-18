3c7770d229ee6f4003873e56f5b2d2b5c5f2f766
TomasAbat <tomasabat2004@gmail.com>
2026-06-17 23:47:11 -0300

    Corrijo e implemento cosas nuevas
    Eliminé la carpeta Sounds, no sirve para cargar las canciones desde el código, le quito un componente AudioSource al AudioManager porque no necesita 2, implemento el metodo reproducir sonido y reproducir música, corrijo el error de que el AudioSource no creaba una instancia suya en otros objetos (Ahora sus metodos publicos pueden usarse en todos los scripts) y pruebo todos los cambios antes mencionados (La parte del código que hace en el Update se va a borrar a futuro)

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,26 +5,45 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
-    public AudioSource cancionMomia;
-    public AudioSource voice1;
-    // Start is called before the first frame update
+    public AudioSource audioSource;
+    AudioClip cancionMomia;
+    AudioClip voice1;
     void Start()
     {
-        
+        if (instance == null){
+            instance = this;
+        }
+        audioSource = gameObject.GetComponent<AudioSource>();
+        cancionMomia = Resources.Load<AudioClip>("CancionMomiaPrueba");
+        voice1 = Resources.Load<AudioClip>("Voice1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace))
-        {
-            cancionMomia.PlayOneShot(cancionMomia.clip);
-            cancionMomia.volume = 0.30f;
-        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            voice1.PlayOneShot(voice1.clip);
+            ReproducirMusica(cancionMomia);
         }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ReproducirSonido(voice1);
+        }
+    }
+
+    public void ReproducirMusica(AudioClip cancionAReproducir)
+    {
+        if(audioSource.isPlaying && audioSource.clip == cancionAReproducir)
+        {
+            return;
+        }
+        audioSource.clip = cancionAReproducir;
+        audioSource.Play();
+    }
+
+    public void ReproducirSonido(AudioClip cancionAReproducir)
+    {
+        audioSource.PlayOneShot(cancionAReproducir);
     }
 
 }
